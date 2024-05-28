5f3fcd71c9258eca9734ae8db4a80c20b4b9e027
Rayna Bhattacharyya <raynabhattacharyya@gmail.com>
2024-05-28 13:11:34 -0700

    fetching all user posts

--- frontend/vite-project/src/roots/Forum.jsx
+++ frontend/vite-project/src/roots/Forum.jsx
@@ -1,9 +1,36 @@
+import { useState, useEffect } from 'react'
 import React from 'react'
+import axios from "axios";
 
 export const Forum = () => {
+  const [posts, setPosts] = useState([])
+
+  async function fetchData() {
+    const res = (await axios.get("http://localhost:5001/forum")).data;
+    console.log(res);
+    setPosts(res);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div>
       <h1>Forum Page</h1>
+      {
+        posts.map((post) => {
+          return <h3>{post["title"]}</h3>
+        })
+      }
+       {/* <label>Post title</label>
+          <input type="text"></input>
+          <br></br>
+          <br></br>
+          <label>Post content</label>
+          <input type="text"></input>
+          <br></br>
+          <br></br> */}
     </div>
   )
 }
