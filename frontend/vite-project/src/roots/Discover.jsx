71118ee1ef8e6a8f93ba9c7db16e860b031f72c7
Sara Inoue <sarainoue11@gmail.com>
2024-05-29 05:04:05 -0800

    added mock profiles to test adaptability of changing size of browser

--- frontend/vite-project/src/roots/Discover.jsx
+++ frontend/vite-project/src/roots/Discover.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import {useState} from 'react'
 
+
+import '../styles/Discover.css'
+import mockphoto from '../images/mockprofilephoto.png'
 export const Discover = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
+    
     <div>
+      {/* HEADER SECTION WITH DISCOVER AND FOLLOWING TABS */}
+      <header>
+        <div className={`links-container ${menuOpen ? 'show' : ''}`}>
+          <Link className="discover-link" to="/discover">Discover</Link>
+          <Link className="following-link" to="/discover/following">Following</Link>
+        </div>
+      </header>  
+
+      
       <h1>Discover Page</h1>
+      <div className= "Discography">
+
+        <div className='IndividualProfile'>
+          <img className="profile-image" src={mockphoto} alt='mockPhoto'/>
+          <div className="profile-name">Sara's Profile</div>
+          <div className="profile-chat-icon">💬</div>
+        </div>
+        <div className='IndividualProfile'>
+          <img className="profile-image" src={mockphoto} alt='mockPhoto'/>
+          <div className="profile-name">Sara's Profile</div>
+          <div className="profile-chat-icon">💬</div>
+        </div>        <div className='IndividualProfile'>
+          <img className="profile-image" src={mockphoto} alt='mockPhoto'/>
+          <div className="profile-name">Sara's Profile</div>
+          <div className="profile-chat-icon">💬</div>
+        </div>
+      </div>
+      
     </div>
   )
 }
