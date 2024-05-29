2b31553821b67f0baaf405d06245433a6a400a0f
Ethan-code-1 <ebo4dq@virginia.edu>
2024-05-29 11:21:55 -0400

    Pushing code for displaying my posts and all posts.

--- frontend/vite-project/src/roots/Forum.jsx
+++ frontend/vite-project/src/roots/Forum.jsx
@@ -3,19 +3,23 @@ import '../styles/Forum.css';
 import axios from "axios";
 
 export const Forum = () => {
+  const [posts, setPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
 
-  const [myPosts, setMyPosts] = useState([])
-
-  async function fetchData() {
+  async function fetchAllPosts() {
     const res = (await axios.get("http://localhost:5001/forum")).data;
-    console.log(res);
+    setPosts(res);
+  }
+
+  async function fetchMyPosts() {
+    const res = (await axios.get("http://localhost:5001/myforum")).data; // Assuming 'myforum' is the route for user-specific posts
     setMyPosts(res);
   }
 
   useEffect(() => {
-    fetchData();
-  }, [])
-
+    fetchAllPosts();
+    fetchMyPosts(); // Fetch user-specific posts on component mount
+  }, []);
 
   const [mode, setMode] = useState('viewAll');
 
@@ -37,108 +41,80 @@ export const Forum = () => {
             }}
             className={`toggle-link ${mode === 'yourPosts' ? 'active' : ''}`}
           >
-            Your Posts
+            <strong>Your Posts</strong>
           </a>
           <span className="separator">|</span>
           <a
             href="#"
-            style = {{marginLeft: '10px'}}
+            style={{marginLeft: '10px'}}
             onClick={(e) => {
               e.preventDefault();
               handleToggle('viewAll');
             }}
             className={`toggle-link ${mode === 'viewAll' ? 'active' : ''}`}
           >
-            View All Posts
+            <strong>View All Posts</strong>
           </a>
           <span className="separator">|</span>
           <a
             href="#"
-            style = {{marginLeft: '10px'}}
+            style={{marginLeft: '10px'}}
             onClick={(e) => {
               e.preventDefault();
               handleToggle('draftNew');
             }}
             className={`toggle-link ${mode === 'draftNew' ? 'active' : ''}`}
           >
-            Draft New Post
+            <strong>Draft New Post</strong>
           </a>
         </div>
       </div>
       <div>
         {mode === 'yourPosts' ? (
           <div className="postsOutsideContainer">
-          <h3>Your Posts</h3>
-          <hr className="forumPageHr" />
-          {myPosts.map(post => (
-            <div key={post.id} className="postWrapper">
-              <div className="postBar"></div>
-              <div className="postContainer">
-                <div className="postHeader">
-                  <span className="postTitle">{post.Title}</span>
-                  <span className="postAuthor">{post.Author}</span>
+            <h3>Your Posts</h3>
+            <hr className="forumPageHr" />
+            {myPosts.map(post => (
+              <div key={post.id} className="postWrapper">
+                <div className="postBar"></div>
+                <div className="postContainer">
+                  <div className="postHeader">
+                    <span className="postTitle">{post.Title}</span>
+                    <span className="postAuthor">{post.Author}</span>
+                  </div>
+                  <div className="postBody">{post.Content}</div>
+                  <div className="postDate">Posted on {post.Date}</div>
+                  <div className="postUpvotes">Upvotes: {post.Votes}</div>
                 </div>
-                <div className="postBody">{post.Content}</div>
-                <div className="postDate">Posted on {post.Date}</div>
-                <div className="postUpvotes">Upvotes: {post.Votes}</div>
+                <div className="voteButtons">
+                  <button id="vote1" className="voteButton">⬆</button>
+                  <button id="vote2" className="voteButton">⬇</button>
+                </div>
               </div>
-              <div className="voteButtons">
-                <button id="vote1" className="voteButton">⬆</button>
-                <button id="vote2" className="voteButton">⬇</button>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
         ) : mode === 'viewAll' ? (
           <div className="postsOutsideContainer">
             <h3>All Posts</h3>
-
             <hr className="forumPageHr" />
-
-            <div className="postWrapper">
-              <div className="postBar"></div>
-              <div className="postContainer">
-                <div className="postHeader">
-                  <span className="postTitle">Sample Post Title</span>
-                  <span className="postAuthor">John Doe</span>
+            {posts.map(post => (
+              <div key={post.id} className="postWrapper">
+                <div className="postBar"></div>
+                <div className="postContainer">
+                  <div className="postHeader">
+                    <span className="postTitle">{post.Title}</span>
+                    <span className="postAuthor">{post.Author}</span>
+                  </div>
+                  <div className="postBody">{post.Content}</div>
+                  <div className="postDate">Posted on {post.Date}</div>
+                  <div className="postUpvotes">Upvotes: {post.Votes}</div>
                 </div>
-                <div className="postBody">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <div className="voteButtons">
+                  <button id="vote1" className="voteButton">⬆</button>
+                  <button id="vote2" className="voteButton">⬇</button>
                 </div>
-                <div className="postDate">Posted on May 28, 2024</div>
-                <div className="postUpvotes">Upvotes: 456</div>
               </div>
-              <div className="voteButtons">
-                <button id="vote1" className="voteButton">⬆</button>
-                <button id="vote2" className="voteButton">⬇</button>
-              </div>
-            </div>
-
-
-
-            <hr className="forumPageHr" />
-
-            <div className="postWrapper">
-              <div className="postBar"></div>
-              <div className="postContainer">
-                <div className="postHeader">
-                  <span className="postTitle">Sample Post Title</span>
-                  <span className="postAuthor">John Doe</span>
-                </div>
-                <div className="postBody">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </div>
-                <div className="postDate">Posted on May 28, 2024</div>
-                <div className="postUpvotes">Upvotes: 42</div>
-              </div>
-              <div className="voteButtons">
-                <button id="vote1" className="voteButton">⬆</button>
-                <button id="vote2" className="voteButton">⬇</button>
-              </div>
-            </div>
-
-
-
+            ))}
           </div>
         ) : (
           <div className="postsOutsideContainer">
@@ -146,12 +122,10 @@ export const Forum = () => {
             <hr className="forumPageHr" />
 
             <form>
-              <input id = "titleField" type="text" placeholder="Title" className="inputField"/>
-              <textarea id = "contentField" placeholder="Content" className="inputField"/>
-              <button id = "submitForum" type="submit" className="submitButton">Submit Post</button>
+              <input id="titleField" type="text" placeholder="Title" className="inputField"/>
+              <textarea id="contentField" placeholder="Content" className="inputField"/>
+              <button id="submitForum" type="submit" className="submitButton">Submit Post</button>
             </form>
-
-
           </div>
         )}
       </div>
