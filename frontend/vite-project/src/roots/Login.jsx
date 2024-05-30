4b24028be7d7fe074c1f7ae9761819d76d7b06d0
Ethan-code-1 <ebo4dq@virginia.edu>
2024-05-30 10:14:41 -0400

    adjusted margins on login page.

--- frontend/vite-project/src/roots/Login.jsx
+++ frontend/vite-project/src/roots/Login.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Auth from '../components/Auth'
+import { AuthContext } from "../components/AuthContext";
 import '../styles/Login.css'
 
 export const Login = () => {
+
+  
   return (
     <div className="auth-container">
       <div className="gradientBorder">
         <div className="innerAuthContainer">
           <h1 className = 'spotimyHeader'>SpotiMy
-            <img src="/spotifyIcon.png" alt="spotify icon" style={{ maxWidth: '50px', height: 'auto', marginLeft: '10px' }} />
+            <img src="/spotifyIcon.png" alt="spotify icon" style={{ maxWidth: '80px', height: 'auto', marginLeft: '10px' }} />
           </h1>
           <br></br>
           
-          <p className="subheading">Log in and start exploring music!</p>
+          <p className="subheading">Log in to start chatting with others and build your own music community!</p>
 
           <hr className="loginpagehr"/>
 
