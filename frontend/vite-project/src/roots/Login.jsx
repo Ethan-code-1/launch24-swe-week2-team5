b6dc89ea65a8531670b403b930bc63721b6990af
Ethan-code-1 <ebo4dq@virginia.edu>
2024-05-30 10:05:32 -0400

    finished login page design.

--- frontend/vite-project/src/roots/Login.jsx
+++ frontend/vite-project/src/roots/Login.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import Auth from '../components/Auth'
+import '../styles/Login.css'
+
 export const Login = () => {
   return (
-    <div>
-      <Auth/>
+    <div className="auth-container">
+      <div className="gradientBorder">
+        <div className="innerAuthContainer">
+          <h1 className = 'spotimyHeader'>SpotiMy
+            <img src="/spotifyIcon.png" alt="spotify icon" style={{ maxWidth: '50px', height: 'auto', marginLeft: '10px' }} />
+          </h1>
+          <br></br>
+          
+          <p className="subheading">Log in and start exploring music!</p>
 
+          <hr className="loginpagehr"/>
+
+          <br></br>
+          <br></br>
+
+          <Auth/>
+        </div>
+      </div>
     </div>
   )
 }
-
 
 export default Login;
