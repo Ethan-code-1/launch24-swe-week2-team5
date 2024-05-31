2a6ad9dc178898e52bbf0bcabf6f2883fbcf97b0
Sara Inoue <sarainoue11@gmail.com>
2024-05-31 05:07:31 -0800

    added loading screen for inbox page.

--- frontend/vite-project/src/roots/Inbox.jsx
+++ frontend/vite-project/src/roots/Inbox.jsx
@@ -6,21 +6,26 @@ import DeleteIcon from '@mui/icons-material/Delete'; // Import the Delete icon
 import TextField from "@mui/material/TextField";
 import { Autocomplete } from "@mui/material";
 
-export const Inbox = ({toId='x'}) => {
+export const Inbox = ({ toId = 'x' }) => {
   const [mode, setMode] = useState('yourPosts'); 
   const [selectedMessage, setSelectedMessage] = useState(null);
   const [messages, setMessages] = useState([]); 
   const [newMsg, setNewMsg] = useState({});
   const [users, setUsers] = useState([]);
   const [to, setTo] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const id = localStorage.getItem("id");
 
   async function fetchAllPosts() {
+    setLoading(true); // Set loading to true before fetching
     const res = (await axios.get(`http://localhost:5001/inbox/${id}`)).data;
     console.log(res);
     setMessages(res);
-    if (res.length > 0) setSelectedMessage(res[0]); 
+    if (res.length > 0) {
+      setSelectedMessage(res[0]); 
+    }
+    setLoading(false); // Set loading to false after fetching
   }
 
   async function fetchAllUsers() {
@@ -28,14 +33,14 @@ export const Inbox = ({toId='x'}) => {
     console.log(res);
     setUsers(res["usernames"]);
     setTo(res["username"]);
-    setNewMsg({...newMsg, "to": res["username"]})
+    setNewMsg({ ...newMsg, "to": res["username"] });
   }
 
   useEffect(() => {
     fetchAllUsers();
     fetchAllPosts();
-    if (toId != 'x') {
-      setMode("viewAll")
+    if (toId !== 'x') {
+      setMode("viewAll");
     }
   }, []);
 
@@ -82,7 +87,7 @@ export const Inbox = ({toId='x'}) => {
           <span className="separator">|</span>
           <a
             href="#"
-            style = {{marginLeft: '.75vw'}}
+            style={{ marginLeft: '.75vw' }}
             onClick={(e) => {
               e.preventDefault();
               handleToggle('viewAll');
@@ -96,105 +101,103 @@ export const Inbox = ({toId='x'}) => {
       <div>
         {mode === 'yourPosts' ? (
           <div className="messagesLayout">
-            <div className="messageList">
-              {messages.map(msg => (
-                <div key={msg.id} onClick={() => setSelectedMessage(msg)} className="messagePreview">
-                    <div className="iconAndText">
+            {loading ? (
+              <div className="loadingscreen"></div>
+            ) : (
+              <>
+                <div className="messageList">
+                  {messages.map(msg => (
+                    <div key={msg.id} onClick={() => setSelectedMessage(msg)} className="messagePreview">
+                      <div className="iconAndText">
                         <PersonIcon className="personIcon"></PersonIcon>
                         <span>
-                        <strong>{msg.From}</strong>: {msg.Title.length > 20 ? msg.Title.substring(0, 17) + '...' : msg.Title}
+                          <strong>{msg.From}</strong>: {msg.Title.length > 20 ? msg.Title.substring(0, 17) + '...' : msg.Title}
                         </span>
+                      </div>
                     </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-            <div className="messageSeparator"></div> 
-            <div className="messageContent">
-              {selectedMessage ? (
-                <>
-                  <div className="messageHeader">
-                    <h3>Topic: {selectedMessage.Title}</h3>
-                    <DeleteIcon className="deleteIcon" onClick={handleDelete} /> {/* Add the trash icon here */}
-                  </div>
-                  <hr className='forumPageHr'></hr>
-                  <h5>From: {selectedMessage.From}</h5>
-                  <br></br>
-                  <h5>{selectedMessage.Content}</h5>
-                </>
-              ) : <p className="emptyInboxMessage">Your message inbox is empty!</p>}
-            </div>
+                <div className="messageSeparator"></div> 
+                <div className="messageContent">
+                  {selectedMessage ? (
+                    <>
+                      <div className="messageHeader">
+                        <h3>Topic: {selectedMessage.Title}</h3>
+                        <DeleteIcon className="deleteIcon" onClick={handleDelete} /> {/* Add the trash icon here */}
+                      </div>
+                      <hr className='forumPageHr'></hr>
+                      <h5>From: {selectedMessage.From}</h5>
+                      <br></br>
+                      <h5>{selectedMessage.Content}</h5>
+                    </>
+                  ) : <p className="emptyInboxMessage">Your message inbox is empty!</p>}
+                </div>
+              </>
+            )}
           </div>
         ) : (
           <div className="postsOutsideContainer">
             <h3>Draft Message</h3>
             <hr className="forumPageHr" />
             <form onSubmit={handleSubmit}>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={users}
-              value={to}
-              sx={{
-                "& .MuiInputBase-root": {
-                  background: '#333',
-                  borderRadius: 1,
-                  padding: '10px',
-                  marginBottom: '20px',
-                  color: 'white',
-                  "&::placeholder": {
-                    color: 'rgba(255, 255, 255, 0.5)', 
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={users}
+                value={to}
+                sx={{
+                  "& .MuiInputBase-root": {
+                    background: '#333',
+                    borderRadius: 1,
+                    padding: '10px',
+                    marginBottom: '20px',
+                    color: 'white',
+                    "&::placeholder": {
+                      color: 'rgba(255, 255, 255, 0.5)', 
+                    },
                   },
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: 'transparent',
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: 'transparent',
+                    },
+                    "&:hover fieldset": {
+                      borderColor: 'rgba(255, 255, 255, 0.5)',
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: 'rgba(255, 255, 255, 0.5)',
+                      boxShadow: '0 0 0 3px rgba(138, 43, 226, 0.5), 0 0 0 4px #8A2BE2, 0 0 0 5px rgba(255, 0, 255, 0.5)',
+                    },
                   },
-                  "&:hover fieldset": {
-                    borderColor: 'rgba(255, 255, 255, 0.5)',
+                  "& .MuiAutocomplete-inputRoot": {
+                    padding: 0, 
                   },
-                  "&.Mui-focused fieldset": {
-                    borderColor: 'rgba(255, 255, 255, 0.5)',
-                    boxShadow: '0 0 0 3px rgba(138, 43, 226, 0.5), 0 0 0 4px #8A2BE2, 0 0 0 5px rgba(255, 0, 255, 0.5)',
+                  "& .MuiAutocomplete-endAdornment": {
+                    color: 'white', 
                   },
-                },
-                "& .MuiAutocomplete-inputRoot": {
-                  padding: 0, 
-                },
-                "& .MuiAutocomplete-endAdornment": {
-                  color: 'white', 
-                },
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  placeholder="Username" 
-                  InputProps={{
-                    ...params.InputProps,
-                    style: { padding: '10px' }, 
-                  }}
-                />
-              )}
-              onChange={(e, value) => { setNewMsg({ ...newMsg, "to": value }) }}
-            />
-
-            {/*
-              <input id="recipField" 
-                type="text" 
-                placeholder="To" 
-                className="inputField" 
-                onChange={(e) => {setNewMsg({...newMsg, "to": e.target.value})}}/> 
-        */} 
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    placeholder="Username" 
+                    InputProps={{
+                      ...params.InputProps,
+                      style: { padding: '10px' }, 
+                    }}
+                  />
+                )}
+                onChange={(e, value) => { setNewMsg({ ...newMsg, "to": value }) }}
+              />
               <input id="titleField" 
                 type="text" 
                 placeholder="Title" 
                 className="inputField" 
-                onChange={(e) => {setNewMsg({...newMsg, "title": e.target.value})}}/>
+                onChange={(e) => { setNewMsg({ ...newMsg, "title": e.target.value }) }}/>
               <textarea id="contentField" 
                 placeholder="Content" 
                 className="inputField"
                 onChange={(e) => {
                   console.log(newMsg);
-                  setNewMsg({...newMsg, "content": e.target.value})
+                  setNewMsg({ ...newMsg, "content": e.target.value })
                 }}/>
               <button id="submitForum" type="submit" className="submitButton">Send Message</button>
             </form>
